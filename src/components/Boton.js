22c30aeb0b6ddc07f52699c6fcaa99158138523d
Bitxejo <bitxogm1976@gmail.com>
2023-10-07 19:37:48 +0200

    first commit

---
+++ src/components/Boton.js
@@ -0,0 +1,19 @@
+import React from "react";
+import  '../style-sheet/Boton.css';
+
+function Boton ( props ) {
+
+const esOperador = valor => {
+  return isNaN ( valor ) && ( valor !=='.' ) && ( valor !== ' = ');
+};
+
+  return (
+    <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : null }`.trimEnd()}>
+      { props.children }
+      
+    </div>
+  );
+
+}
+
+export default Boton;
